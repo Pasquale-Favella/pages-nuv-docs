17e582c672b86ceda87ae19012c96c063ce21fa6
Favella Pasquale <FavellaPa@valueteam.com>
2022-03-24 10:42:08 +0100

    fix for assets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/pages-nuv-docs",
+  assetPrefix: "/pages-nuv-docs",
 }
 
 module.exports = nextConfig
