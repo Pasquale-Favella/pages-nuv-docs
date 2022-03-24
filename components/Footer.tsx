f9c1856359d8b2843daf558bc911d378a1fbd762
Favella Pasquale <FavellaPa@valueteam.com>
2022-03-24 11:23:37 +0100

    fix for image card resurce

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = ()=>{
         <footer className="text-gray-600 body-font ">
           <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-primary">
-              <img src={'/nuv-logo.png'} className="w-12 h-12 p-2 rounded-full"/>
+              <img src={`${process.env.NEXT_PUBLIC_ASSETS || ''}/nuv-logo.png`} className="w-12 h-12 p-2 rounded-full"/>
               <span className="ml-3 text-xl">Nuvolaris</span>
             </a>
             <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2021 Nuvolaris —
