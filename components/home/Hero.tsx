bf3f34b3f902dc77b5ead4f7d986fe1140fa83a4
Favella Pasquale <FavellaPa@valueteam.com>
2022-03-24 11:06:58 +0100

    hero image public asset

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = ()=>{
         <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-            <img className="object-cover object-center rounded" width={600} height={600} alt="hero" src="/launch.svg"/>
+            <img className="object-cover object-center rounded" width={600} height={600} alt="hero" src={`${process.env.NEXT_PUBLIC_ASSETS || ''}/launch.svg`}/>
             </div>
             <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-primary">Nuvolaris 
