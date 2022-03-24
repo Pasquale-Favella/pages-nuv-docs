972dd3f9613ba750a8d65358fdca35fb56ec6e14
Favella Pasquale <FavellaPa@valueteam.com>
2022-03-24 15:27:52 +0100

    try support for PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     loader: "akamai",
@@ -7,6 +9,11 @@ const nextConfig = {
   },
   basePath: `${process.env.NEXT_PUBLIC_ASSETS || ''}`,
   assetPrefix: `${process.env.NEXT_PUBLIC_ASSETS || ''}`,
-}
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  }
+})
 
 module.exports = nextConfig
